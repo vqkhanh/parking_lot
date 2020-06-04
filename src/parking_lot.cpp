cdc92d5f146d11e06badfac2443195f3094dbf16
khanh.vq@onehealth.vn <khanh.vq@onehealth.vn>
2020-06-05 03:34:36 +0700

    update write file

--- src/parking_lot.cpp
+++ src/parking_lot.cpp
@@ -1,3 +1,10 @@
+//
+//  main.cpp
+//
+//  Created by Vo Khanh on 6/4/20.
+//  Copyright © 2020 khanhvo. All rights reserved.
+//
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -9,74 +16,130 @@ using namespace std;
 #define CM_LV "leave"
 #define CM_ST "status"
 #define EMPTY_STR " "
+#define FULL_STR "Sorry, parking lot is full"
+#define PRICE 10
+#define EX_PRICE 10
+#define REGULAR_HOURS 2
 
 fstream f2;
+unsigned int nLots = 0;
+
+typedef struct lot{
+    bool status;
+    string carName;
+};
+
+lot *lots;
 
 void write_line(string data){
-	f2 << data;
+    f2 << data;
 }
 
 void get_CM(string line){
-	int32_t index = line.find(EMPTY_STR);
-		//int32_t search_index2 = line.find(CM_PR);
-
-		if(index != -1){
-			string str = line.substr(0,index);
-			//cout << str;
-			//cout << line.at(line.length() - 1);
-			if(str == CM_CR){
-				//cout << "create ";
- 				string ch = line.substr(index, line.length() - 1);
-				string str_out = "Created parking lot with ";
-				str_out += ch;
-				str_out += " lots";
-				cout << str_out;
-				write_line(str_out);
-			}else if(str == CM_PR){
-				//cout << "park ";
-			}else if(str == CM_LV){
-				//cout << "leave ";
-			}
-		}else{
-			if(line == CM_ST){
-				//cout << "status";
-			}
-		}
+    int32_t index = line.find(EMPTY_STR);
+    string str = line.substr(0,index);
+    if(index != -1){
+        if(str == CM_CR){
+            string ch = line.substr(index);
+            string str_out = "Created parking lot with";
+            str_out += ch;
+            str_out += " lots";
+            nLots = stoi(ch);
+            lots = new lot[nLots];
+            write_line(str_out);
+            for(int i = 0; i < nLots; i++){
+                lots[i].status = false;
+            }
+        }else if(str == CM_PR){
+            if(nLots == 0){
+                write_line(FULL_STR);
+            }else{
+                string res_str = line.substr(index + 1);
+                int i = 0;
+                for(; i < nLots; i++){
+                    if(lots[i].status == false){
+                        lots[i].status = true;
+                        lots[i].carName = res_str;
+                        string str_out = "\nAllocated slot number: ";
+                        str_out += to_string(i + 1);
+                        write_line(str_out);
+                        break;
+                    }
+                }
+                if(i == nLots){
+                     write_line("\nSorry, parking lot is full");
+                }
+            }
+        }else if(str == CM_LV){
+            int32_t l_index = line.rfind(EMPTY_STR);
+            string str_middle = line.substr(index + 1,l_index - index - 1);
+            string res_str = line.substr(l_index + 1);
+            int i = 0;
+            for(; i < nLots; i++){
+               if(lots[i].status == true && str_middle.compare(lots[i].carName) == 0){
+                   lots[i].status = false;
+                   lots[i].carName = "";
+                   string str_out = "\nRegistration number ";
+                   str_out += str_middle;
+                   str_out += " with Slot Number ";
+                   str_out += to_string(i + 1);
+                   str_out += " is free with Charge ";
+                   int times =  stoi(res_str);
+                   int charge = (times <= REGULAR_HOURS) ? PRICE : (PRICE + ((times - REGULAR_HOURS) * EX_PRICE));
+                   str_out += to_string(charge);
+                   write_line(str_out);
+                   break;
+               }
+            }
+            if(i == nLots){
+                string str_out = "\nRegistration number ";
+                str_out += str_middle;
+                str_out += "  not found";
+                write_line(str_out);
+            }
+        }
+    }else{
+        if(line == CM_ST){
+            write_line("\nSlot No. Registration No.");
+            for(int i = 0; i < nLots; i++){
+                if(lots[i].status == true){
+                    string str_out = "\n";
+                    str_out += to_string(i + 1);
+                    str_out += " ";
+                    str_out += lots[i].carName;
+                    write_line(str_out);
+                 }
+            }
+        }
+    }
 }
 
 void read_input(){
-	fstream f;
-	f.open("input.txt", ios::in);
+    fstream f;
+    f.open("ip.txt", ios::in);
+    if (f.fail()) {
+        // file could not be opened
 
-	//fstream f2;
-	f2.open("output.txt", ios::out);
+    }else{
+     
+    }
+    //fstream f2;
+    f2.open("output.txt", ios::out);
  
-	string data;
+    string data;
  
-	string line;
-	while (!f.eof())
-	{
-		getline(f, line);
-		int32_t index = line.find(CM_CR);
-		//int32_t search_index2 = line.find(CM_PR);
-		//if(index != -1){
-		//	cout << line.at(line.length() - 1);
-			get_CM(line); 
-		//}
-		//cout << search_index;
-
-		//cout << search_index2;
-		//data += line;
-	}
- 
-	f.close();
- 
-	//cout << data;
-	//cout << CM_CRE;
+    string line;
+    while (!f.eof())
+    {
+        getline(f, line);
+        get_CM(line);
+    }
+    f.close();
 }
  
-int main()
+int main(int argc, const char * argv[])
 {
-	read_input();
-	write_file();
+    read_input();
+    return 0;
 }
+
